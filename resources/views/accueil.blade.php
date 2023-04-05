d1c260c3cca4672a55a9d7ad98bf6e39d6dcb914
Kdichon <herve@kdichon.fr>
2023-04-05 18:52:22 -0400

    tailwind css

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -10,20 +10,31 @@
                 @csrf
                     <div class="flex mt-4">
                     <input name="task" class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-white-darker" placeholder="Ajouter votre tâche">
-                    <button class="flex-no-shrink p-2 border-2 text-teal border-teal hover:text-red hover:bg-teal btn btn-primary btn-lg ms-2 rounded hover:rounded-lg">Add</button>
+                    <button class="flex-no-shrink p-1 border-teal hover:text-red hover:bg-teal rounded-full hover:rounded-lg"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                      </svg>
+                    </button>
                 </div>
             </form>
         </div>
         <div>
             @forelse ($todos as $itemTodo )
                 <div class="flex mb-4 items-center">
-                    @if ($itemTodo->status == 'n')
+                    <!--@if ($itemTodo->status == 'n')
                         <p class="w-full text-grey-darkest">{{$itemTodo->task}}</p>
                     @else
                         <p class="w-full text-grey-darkest line-through">{{$itemTodo->task}}</p>
-                    @endif
-                    <a href="{{ route('upd', $itemTodo->id)}}" class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-red text-green border-green hover:bg-green"> {{$itemTodo->status=='o'?'Not':''}} Done</a>
-                    <a href="{{ route('del', $itemTodo->id)}}" class="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-red hover:bg-red">Remove</a>
+                    @endif-->
+                    <p class="w-full text-grey-darkest {{$itemTodo->status=='o'?'line-through':''}}">{{$itemTodo->task}}</p>
+                    <a href="{{ route('upd', $itemTodo->id)}}" class="flex-no-shrink p-1 ml-4 mr-2 border-2 rounded-full bg-green-500 border-black hover:bg-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 bg-green">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                      </svg>
+                      </a>  <!--  {{$itemTodo->status=='o'?'Not':''}} Done -->
+                    <a href="{{ route('del', $itemTodo->id)}}" class="flex-no-shrink p-1 ml-2 border-2 rounded-full bg-red-500 border-black hover:bg-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                      </svg>
+                      </a>
                 </div>
             @empty
                 <div class="flex mb-4 items-center">
